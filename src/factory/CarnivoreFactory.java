e5e247dd5710ed23edca3d86dbae4b22828c39c0
Hen Simkin <hensi1@ac.sce.ac.il>
2022-05-30 14:18:09 +0300

    a lot of changes been done

--- src/factory/CarnivoreFactory.java
+++ src/factory/CarnivoreFactory.java
@@ -1,4 +1,16 @@
 package factory;
 
-public class CarnivoreFactory {
+import animal.Animal;
+
+public class CarnivoreFactory extends AbstractFactory
+{
+
+    @Override
+    Animal getAnimal(String animalType)
+    {
+        if(animalType=="Lion")
+        {
+
+        }
+    }
 }
